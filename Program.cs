fac53092b0464a60d273b4bc10caccd833bf5408
Dmitriy209 <dim286754@gmail.com>
2024-06-16 10:50:06 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace controlExit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopMessage = null;
+            string stopWord = "exit";
+            bool stopCycle = true;
+
+            Console.WriteLine("Вы входите в заведение.\nЕсли хотите выйти напишите exit.");
+
+            while (stopWord != stopMessage)
+            {
+                Console.WriteLine("Мартышка хлопает в тарелки.");
+                Console.WriteLine("Введите стоп слово:");
+
+                stopMessage = Console.ReadLine();
+
+                if (stopWord != stopMessage)
+                {
+                    Console.WriteLine("Вы ввели неверное стоп-слово.");
+                }
+            }
+            Console.WriteLine("Вы покинули заведение и получили футболку в подарок.");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("controlExit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("controlExit")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b01ab073-1b1a-439b-8c40-c04059b541ee")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
